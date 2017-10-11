3055a00d97b02532117114a27e40c85ebbf572d0
Charles <cweng@stuy.edu>
2017-10-10 22:48:00 -0400

    clean up

--- app.py
+++ app.py
@@ -4,23 +4,30 @@ import os
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
-
+# root route
 @app.route("/")
 def root():
-    # check if logged in
-    if not 'loggedin?' in session:
-        session["loggedin?"] = False
-
-    if session['loggedin?']:
+    # Go to welcome if properly logged in
+    if 'loggedin?' in session and session['loggedin?'] == True:
         return redirect("/welcome")
 
-    # need user data
-    return render_template('form.html', text = 'Please enter your login')
+    #else go to /login
+    return redirect('/login')
 
 # get here through form.html
 @app.route("/login", methods = ["POST", "GET"])
 def login():
-    # check credentials
+    # Go to welcome if properly logged in (for people who are messing around with URL)
+    if 'loggedin?' in session and session['loggedin?'] == True:
+        return redirect("/welcome")
+
+    # set log in status to false if it's not there and show login w/o rude wrong credentials message
+    # should only appear once per login
+    if not 'loggedin?' in session:
+        session['loggedin?'] = False
+        return render_template('form.html', text = 'Please enter your login')
+
+    # check credentials against built in value
     usercheck = False
     passcheck = False
     session["user"] = request.form.get("username")
@@ -29,6 +36,7 @@ def login():
     if request.form.get("password") == "13":
             passcheck = True
 
+    # process the credentials + says whats wrong with your credentials
     if (usercheck and passcheck):
         session['loggedin?'] = True
         return redirect("/welcome")
@@ -39,14 +47,25 @@ def login():
     return render_template('form.html', text = "wrong username and password")
 
 
+# get here through valid login check
 @app.route("/welcome")
 def welcome():
+    # are you supposed to be here?
+    if not 'loggedin?' in session or session['loggedin?'] == False:
+        return redirect("/login")
+
     return render_template("welcome.html", username = session['user'])
 
+
+# get here through welcome.html
 @app.route("/signout")
 def signout():
-    session.pop("user")
-    session.pop("loggedin?")
+    # should remove logged in status if possibke
+    if 'loggedin?' in session and session['loggedin?']:
+        session.pop("user")
+        session.pop("loggedin?")
+
+    # go back to root
     return redirect("/")
 
 def debug():
